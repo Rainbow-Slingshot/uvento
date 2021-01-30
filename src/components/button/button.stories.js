e107cb291e860c642dbb1df9fb446d30a23fb8e0
mrana17 <rm_a@msn.com>
2021-01-30 19:17:46 +0100

    Added footer with buttons

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -2,6 +2,8 @@ import "./button.css";
 import buttonText from "./button-text.html";
 import buttonLogo from "./button-logo.html";
 import buttonHome from "./home-button.html";
+import buttonSearch from "./search-button.html";
+import buttonStar from "./star-button.html";
 
 export default {
   title: "Components/Button",
@@ -11,3 +13,5 @@ export default {
 export const withText = () => buttonText;
 export const withLogo = () => buttonLogo;
 export const withHome = () => buttonHome;
+export const withSearch = () => buttonSearch;
+export const withStar = () => buttonStar;
