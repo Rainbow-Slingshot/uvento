b52b2af99c51988899aab08e3cbc57130287bf0b
mrana17 <rm_a@msn.com>
2021-02-01 09:26:19 +0100

    cards component added

---
+++ src/components/cards/cards.stories.js
@@ -0,0 +1,9 @@
+import "./cards.css";
+import cardsMic from "./card-mic.html";
+
+export default {
+  title: "Components/cards",
+  parameters: { layout: "centered" },
+};
+
+export const withMic = () => cardsMic;
